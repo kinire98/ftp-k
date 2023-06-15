15e11e38cbec737897b93d0bf38bc9a76a529a9e
kinire98 <93119213+kinire98@users.noreply.github.com>
2023-06-15 17:38:49 +0200

    some things added, improved, and started connections

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,36 @@
+use std::net::ToSocketAddrs;
 
+use ftp::FtpStream;
+use ftp::types::Result;
 
+pub fn open_connection<A: ToSocketAddrs>(addr: A) -> Result<FtpStream> {
+    FtpStream::connect(addr)
+}
 
-pub mod cmd {
-    pub fn proccess(cmd: &str) {
+pub struct Connection {
+    connection: FtpStream,
+    path: String
+}
+impl Connection {
+    pub fn new(conn: FtpStream) -> Connection {
+        Connection { connection: conn, path: "/".to_string() }
+    }
+    pub fn proc(&self, cmd: &str) {
         let cmd = cmd.to_lowercase();
         let cmd = cmd.as_str();
         match cmd {
             "help" => println!("!               delete          literal         prompt          send
-?               debug           ls              put             status
-append          dir             mdelete         pwd             trace
-ascii           disconnect      mdir            quit            type
-bell            get             mget            quote           user
-binary          glob            mkdir           recv            verbose
-bye             hash            mls             remotehelp
-cd              help            mput            rename
-close           lcd             open            rmdir"),
+                ?               debug           ls              put             status
+                append          dir             mdelete         pwd             trace
+                ascii           disconnect      mdir            quit            type
+                bell            get             mget            quote           user
+                binary          glob            mkdir           recv            verbose
+                bye             hash            mls             remotehelp
+                cd              help            mput            rename
+                close           lcd             open            rmdir"),
             "cls" | "clear" => print!("\x1B[2J\x1B[1;1H"),
             _ => println!("Please enter a valid command.\r\nIf you need a list of all the commands, type {:?}", "help"),
         }
     }
 }
+    
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,56 @@
-use std::{thread::{self, sleep}, io::{self, Write}};
+use std::{thread, io::{self, Write}, sync::mpsc::{Sender, Receiver}};
 use std::sync::mpsc;
-use std::time::Duration;
 use spinoff::{Spinner, spinners, Color};
+use ftp::FtpError;
 
 fn main() {
 
     // * Clear the terminal
     print!("\x1B[2J\x1B[1;1H");
+    let mut connection;
+    loop {
+        let mut buffer = String::new();
+        println!("Input the IP or the DNS name of FTP server you are trying to connect to:");
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Read failed");
+        let spinner = Spinner::new(spinners::FistBump, "Connecting to the FTP server...", Color::TrueColor { r:74, g: 246, b: 38 });
+        let (tx, rx): (Sender<Result<bool, FtpError>>, Receiver<Result<bool, FtpError>>) = mpsc::channel(); // * The result is an integer so the compiler stops bothering 
+        let (txr, rxr) = mpsc::channel();
+        let mut buffer_cl = buffer.clone();
+        thread::spawn(move || {
+            let receive = rx.recv().unwrap();
+            print!("\x1B[2J\x1B[1;1H");
+            buffer_cl.pop();
+            buffer_cl.pop();
+            match receive {
+                Ok(_) => {
 
-    let mut buffer = String::new();
-    println!("Input the IP or the DNS name of FTP server you are trying to connect to:");
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Read failed");
-    let spinner = Spinner::new(spinners::FistBump, "Connecting to the FTP server...", Color::TrueColor { r:74, g: 246, b: 38 });
-    let (tx, rx) = mpsc::channel();
-    let (txr, rxr) = mpsc::channel();
-    let mut buffer_cl = buffer.clone();
-    thread::spawn(move || {
-        let _receive = rx.recv().unwrap();
-        print!("\x1B[2J\x1B[1;1H");
-        buffer_cl.pop();
-        buffer_cl.pop();
-        let msg = &format!("Connected to the {} server", buffer_cl);
-        spinner.success(msg);
-        txr.send("").unwrap();
-    });
-    sleep(Duration::from_secs(1));
-    tx.send("").unwrap();
-    rxr.recv().unwrap();
+                    let msg = &format!("Connected to the {} server", buffer_cl);
+                    spinner.success(msg);
+                }
+                Err(e) => {
+                    let msg = &format!("An error has ocurred connecting to the {} server: {}", buffer_cl, e);
+                    spinner.fail(msg);
+                }
+            }
+            txr.send("").unwrap();
+        });
+        connection = ftp_k::open_connection(format!("{buffer}:21"));
+        match connection {
+            Ok(_) => {
+                tx.send(Ok(true)).unwrap();
+                rxr.recv().unwrap();
+                break;
+            },
+            Err(err) => {
+                tx.send(Err(err)).unwrap();
+                rxr.recv().unwrap();
+            }
+        }
+    }
+    let connection = ftp_k::Connection::new(connection.unwrap());
+
     loop {
         let mut cmd = String::new();
         print!("ftp> ");
@@ -38,7 +60,7 @@ fn main() {
             .expect("Read failed");
         cmd.pop();
         cmd.pop();
-        ftp_k::cmd::proccess(&cmd);
+        connection.proc(&cmd);
         if cmd != "cls" && cmd != "clear" {println!();}
     }
 }
